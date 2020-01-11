e9898ad8691d4c17b40c6abe67d0885e35f18e43
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2020-01-11 12:35:19 +0100

    2

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+const User = require("./User");
 
+const app = express();
+
+const dbPath = "mongodb+srv://admin:chatserverAdmin@chatserver-cluster-eejku.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(dbPath, {
+    useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", () => {
+    console.log("> error occurred from the database");
+});
+db.once("open", () => {
+    console.log("> successfully opened the database");
+});
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(5000, () => {
+    console.log("listenlog")
+})
+
+app.get("/", (req, res) => {
+    User.find({})
+    .then((User) => {
+        if(!User) res.send('Ingen users');
+        res.json(User);
+    })
+    .catch(err => console.log(err));
+});
